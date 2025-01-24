14d3424b0d45e1b4a2db767c69c52681191f00b2
GamerM92 <manuel_marx@outlook.de>
2025-01-24 12:09:47 +0100

    1. Bird Klasse

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -7,11 +7,13 @@ public class App {
 
         Queen queen = new Queen();
         Worker worker = new Worker();
-        Drone drone = new Drone(); 
+        Drone drone = new Drone();
+        Bird bird = new Bird();
 
         pollObj(queen);
         pollObj(worker);
         pollObj(drone);
+        pollObj(bird);
 
         // HoneyBee ist abstrakt --> Fehler!
         // HoneyBee bee = new HoneyBee(); 
